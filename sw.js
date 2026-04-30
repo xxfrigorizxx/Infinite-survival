c3191ed193c4c63663620b60c85f5dfdfa61cac8
xxfrigorizxx <xxfrigorizxx@gmail.com>
2026-04-29 22:08:51 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'survivor-v8.1.1-hotfix-recup-progression-2026-04-29';
+const CACHE = 'survivor-v8.1.3-update-orbes-protectrices-2026-04-29';
 const ASSETS = [
   './',
   './index.html',
